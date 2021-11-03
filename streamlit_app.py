3a8467ed930fa624d5c7f5af26d7630434bec4aa
Jspaezp <jspaezp@gmail.com>
2021-11-03 12:36:44 -0400

    initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,58 @@
+import logging
+import os
+import torch
+import elfragmentador as ef
+from elfragmentador.model import PepTransformerModel
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import base64
+from io import BytesIO
+
+from matplotlib.figure import Figure
+from matplotlib import pyplot as plt
+
+import streamlit as st
+
+import logging
+
+logging.basicConfig(
+    filename="app.log",
+    filemode="w",
+    level=logging.INFO,
+    format="%(name)s - %(levelname)s - %(message)s",
+)
+
+
+def make_spec_fig(spectrum):
+    # Generate the figure **without using pyplot**.
+    fig = Figure(figsize=(8, 4))
+    ax = fig.subplots()
+    spectrum.plot(ax=ax)
+
+    return fig
+
+
+st.title("ElFragmentador")
+
+model = PepTransformerModel.load_from_checkpoint(ef.DEFAULT_CHECKPOINT)
+model.eval()
+
+sequence = st.text_input("Peptide Sequence", value = 'MYPEPTIDEK')
+charge = st.number_input('Charge', min_value = 1, max_value = 5, value = 3)
+nce = st.number_input('nce', min_value = 20.0, max_value = 40.0, value = 32.0, step = 0.1)
+
+
+with torch.no_grad():
+    pred = model.predict_from_seq(
+        sequence, charge=int(charge), nce=float(nce), as_spectrum=True
+    )
+
+print(pred)
+fig = make_spec_fig(pred)
+
+
+st.pyplot(fig)
+
+
