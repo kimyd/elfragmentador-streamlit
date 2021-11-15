d5fa6627ad82885cc91ab4eaa2754fecfc6ef629
Jspaezp <jspaezp@gmail.com>
2021-11-14 22:10:02 -0500

    added more text to the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,18 @@ def make_spec_fig(spectrum):
 
 st.title("ElFragmentador")
 
+page_text = """
+
+ElFragmentador implements a neural net that leverages the transformer architecture to predict peptide properties (retention time and fragmentation).
+
+Currently the documentation lives here: [https://jspaezp.github.io/elfragmentador/](https://jspaezp.github.io/elfragmentador/)
+
+Please check out [The Quickstart guide](https://jspaezp.github.io/elfragmentador/quickstart) for usage instructions on your local system.
+Feel free to use this web app to check out how the model works.
+"""
+
+st.markdown(page_text)
+
 model = PepTransformerModel.load_from_checkpoint(ef.DEFAULT_CHECKPOINT)
 model.eval()
 
